f640119ada3f0eba0df3fd9ff84d33ae88eb4c4e
Single <musicman_leehom@126.com>
2016-11-05 18:46:03 +0800

    Initial Commit

---
+++ SGFoundation/SGFoundation.h
@@ -0,0 +1,19 @@
+//
+//  SGFoundation.h
+//  SGFoundation
+//
+//  Created by Single on 2016/11/5.
+//  Copyright © 2016年 single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGFoundation.
+FOUNDATION_EXPORT double SGFoundationVersionNumber;
+
+//! Project version string for SGFoundation.
+FOUNDATION_EXPORT const unsigned char SGFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGFoundation/PublicHeader.h>
+
+
